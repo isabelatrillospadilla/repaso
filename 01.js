4688b86e56c9f8a1fe2125c0ea8576a788ee15c6
isabelatrillospadilla <isabelatrillospadilla@gmail.com>
2022-12-10 19:04:08 -0300

    realizado

--- 01.js
+++ 01.js
@@ -9,8 +9,14 @@ function soloNumeros(array) {
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
-
   // Tu código aca:
+  var nuevoArray = [];
+  for (var i = 0; i < array.length; i++){
+  if (typeof array[i] === "number"){
+    nuevoArray.push(array[i]);
+  }
+}
+  return nuevoArray;
 
 }
 
